7f439cd8abbbaa4450df3b9c2080832c2c175104
AoEiuV-PC <490674483@qq.com>
2015-12-24 21:41:27 +0800

    commit

--- androidclient/jni/Android.mk
+++ androidclient/jni/Android.mk
@@ -3,5 +3,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= chat-jni.c
 LOCAL_MODULE:= chat-jni 
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+LOCAL_C_INCLUDES += system/core/include/cutils
+LOCAL_SHARED_LIBRARIES := libcutil
 
 include $(BUILD_SHARED_LIBRARY)
--- androidclient/src/com/aoeiuv020/chat/Main.java
+++ androidclient/src/com/aoeiuv020/chat/Main.java
@@ -2,14 +2,107 @@ package com.aoeiuv020.chat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ScrollView;
+import android.view.View;
 
 public class Main extends Activity
 {
     /** Called when the activity is first created. */
+	EditText eip,eport,esendtext;
+	Button bconnect,bsendbutton;
+	ScrollView smessage;
+	String message,ip;
+	int port;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+		try
+		{
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.main);
+
+			eip=(EditText)findViewById(R.id.ip);
+			eport=(EditText)findViewById(R.id.port);
+			esendtext=(EditText)findViewById(R.id.sendtext);
+			bsendbutton=(Button)findViewById(R.id.sendbutton);
+			bconnect=(Button)findViewById(R.id.connect);
+			smessage=(ScrollView)findViewById(R.id.messageview);
+
+			bconnect.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v)
+				{
+					try
+					{
+						ip=eip.getText().toString();
+						port=Integer.parseInt(eport.getText().toString());
+						int err;
+						err=connect(ip,port);
+						toast(errtostr(err));
+					}
+					catch(Exception e)
+					{
+						toast(e.toString());
+					}
+				}
+				void toast(String str)
+				{
+					Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+				}
+			});
+			bsendbutton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v)
+				{
+					try
+					{
+						message=esendtext.getText().toString();
+						int err;
+						err=send(message);
+						if(err!=0)
+						{
+							toast(errtostr(err));
+						}
+						else
+						{
+							esendtext.setText("");
+						}
+					}
+					catch(Exception e)
+					{
+						toast(e.toString());
+					}
+				}
+				void toast(String str)
+				{
+					Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+				}
+			});
+			recv();
+		}
+		catch(Exception e)
+		{
+			toast(e.toString());
+		}
+	}
+	public void toast(String str)
+	{
+		Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+	}
+	public void addmessage(String message)
+	{
+		EditText edittext;
+		edittext=new EditText(getApplicationContext());
+		edittext.setText(message);
+
+	}
+	public native int connect(String ip,int port);
+	public native int send(String message);
+	public native int recv();
+	public native String errtostr(int err);
+	static
+	{
+		System.loadLibrary("chat-jni");
+	}
 }
